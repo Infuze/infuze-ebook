f2648bb8bff0771055c11a8e6a05e5e4bc4dcc9a
Martin Wright <martin@infuze.co.uk>
2019-01-11 17:00:27 +0000

    Restructure files

---
+++ config/webpack.dev.js
@@ -0,0 +1,139 @@
+const path = require('path');
+const webpack = require("webpack");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const autoprefixer = require("autoprefixer");
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+//const NodemonPlugin = require('nodemon-webpack-plugin');
+const merge = require('webpack-merge');
+const TARGET = process.env.npm_lifecycle_event;
+const ARGV = require('yargs').argv;
+
+let common = {
+    entry: ['./src/js/app.js'],
+    output: {
+        filename: 'infuze-ebook.js',
+        path: path.resolve(__dirname, '../lib')
+    },
+    devServer: {
+        contentBase: [
+            path.join(__dirname, '../')
+        ],
+        openPage: '/sample',
+        open: true,
+        watchContentBase: true,
+        overlay: true,
+        stats: {
+            colors: true
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: "babel-loader",
+                        options: {
+                            presets: [
+                                [
+                                    '@babel/preset-env',
+                                    {
+                                        debug: true
+                                    }
+                                ]
+                            ]
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.html/,
+                loader: 'file-loader?name=[name].[ext]'
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    //MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            options: {},
+                            plugins: () => {
+                                autoprefixer({ browsers: ['last 2 versions'] });
+                            }
+                        }
+                    },
+                    'sass-loader']
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                loader: 'file-loader',
+                exclude: /node_modules/,
+                options: {
+                    //name: 'css/fonts/[name].[ext]',
+                    //mimetype: 'application/font-woff',
+                    //name: "css/fonts/[name].[ext]",
+                    name: '[name].[ext]',
+                    outputPath: 'fonts'
+                },
+            }]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'style.css',
+            publicPath: '../'
+            //filename: "css/style.css",
+            //chunkFilename: "[id].css"
+        }),
+        /* new CopyWebpackPlugin([
+            { from: 'src/sample/images', to: '../sample/images' },
+            { from: 'src/sample/libs', to: '../sample/libs' },
+            { from: 'src/sample/iquiz_assets', to: '../sample/iquiz_assets' }
+        ]) */
+        //new NodemonPlugin(),
+        //new webpack.HotModuleReplacementPlugin()
+    ]
+}
+
+
+if (ARGV.mode === 'production') {
+    module.exports = merge(common, {
+        mode: "production",
+        devtool: 'source-map',
+        plugins: [
+            new CleanWebpackPlugin(['lib/**/*'], {
+                root: path.join(__dirname, '..'),
+            })
+        ],
+        optimization: {
+            minimizer: [
+                new UglifyJsPlugin({
+                    cache: true,
+                    parallel: true,
+                    sourceMap: true // set to true if you want JS source maps
+                }),
+                new OptimizeCSSAssetsPlugin({})
+            ]
+        },
+    })
+}
+
+if (ARGV.mode === 'development') {
+    module.exports = merge(common, {
+        mode: "development",
+        devtool: 'source-map',
+        //devtool: 'eval-cheap-module-source-map',
+    })
+}
