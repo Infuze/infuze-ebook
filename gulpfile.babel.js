8b4e9c0ca175d1f59b401f0cad846a14a9ed6dae
Martin Wright <martinw@live.com>
2019-01-23 12:46:02 +0000

    Update gulp process

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,6 +2,8 @@
 import gulp from 'gulp';
 import fs from 'fs';
 import plumber from "gulp-plumber";
+import replace from 'gulp-replace';
+
 const newer = require("gulp-newer");
 const merge = require("merge-stream");
 const zip = require('gulp-zip');
@@ -10,6 +12,10 @@ const packConfig = require("./pack-config.json");
 const sass = require('gulp-sass');
 sass.compiler = require('node-sass');
 const uglify = require("gulp-uglify");
+const concat = require('gulp-concat');
+import autoprefixer from 'gulp-autoprefixer';
+const imagemin = require('gulp-imagemin');
+const changed = require('gulp-changed');
 
 import critical from 'critical';
 import babelify from 'babelify';
@@ -19,6 +25,14 @@ import buffer from 'vinyl-buffer';
 import plugins from 'gulp-load-plugins';
 import source from 'vinyl-source-stream';
 import rename from 'gulp-rename';
+var runSequence = require('run-sequence');
+const htmlmin = require('gulp-htmlmin');
+
+let title = packConfig.packs[0].title;
+console.log('title ', title)
+title = title.replace(/,/g, "");
+title = title.replace(/\s/g, "-");
+let packDir = "pack/" + title;
 
 
 
@@ -78,82 +92,48 @@ gulp.task('scripts-min', () => {
         .pipe(buffer())
         .pipe(uglify())
         .pipe(gulp.dest('./deploy/js/'))
-    //.pipe(browserSync.stream());
-});
-
-/* ----------------- */
-/* Jsmin
-/* ----------------- */
-gulp.task('jsmin', () => {
-    var envs = plugins().env.set({
-        'NODE_ENV': 'production'
-    });
-
-    return browserify({
-        'entries': ['./src/example/js/index.js'],
-        'debug': false,
-        'transform': [
-            babelify.configure({
-                'presets': ['es2015', 'react']
-            })
-        ]
-    })
-        .bundle()
-        .pipe(source('bundle.js'))
-        .pipe(envs)
-        .pipe(buffer())
-        .pipe(plugins().uglify())
-        .pipe(envs.reset)
-        .pipe(gulp.dest('./deploy/js/'));
 });
 
 
 /* ----------------- */
 /* SASS
 /* ----------------- */
-
 gulp.task('sass', () => {
-    return gulp.src('./src/example/css/**/*.scss')
+    return gulp.src('./src/example/css/styles.scss')
         .pipe(plugins().sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
+        .pipe(concat('styles.css'))
+        .pipe(autoprefixer())
         .pipe(plugins().sourcemaps.write())
         .pipe(gulp.dest('./build/css/'))
         .pipe(browserSync.stream());
 });
 gulp.task('sass-min', () => {
-    return gulp.src('./src/example/css/**/*.scss')
+    return gulp.src('./src/example/css/styles.scss')
         .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+        .pipe(concat('styles.css'))
+        .pipe(autoprefixer())
         .pipe(gulp.dest('./deploy/css/'))
-    //.pipe(browserSync.stream());
 });
 
-
-/* ----------------- */
-/* Styles
-/* ----------------- */
-gulp.task('styles', () => {
-    return gulp.src('./src/example/css/**/*.scss')
-        .pipe(plugins().sourcemaps.init())
-        .pipe(plugins().sass().on('error', plugins().sass.logError))
-        .pipe(plugins().sourcemaps.write())
-        .pipe(gulp.dest('./build/css/'))
-        .pipe(browserSync.stream());
-});
 
 
 /* ----------------- */
 /* HTML
 /* ----------------- */
-gulp.task('html', ['cssmin'], () => {
-    return gulp.src('src/example/index.html')
-        .pipe(critical.stream({
-            'base': 'build/',
-            'inline': true,
-            'extract': true,
-            'minify': true,
-            'css': ['./build/css/style.css']
-        }))
-        .pipe(gulp.dest('build'));
+gulp.task("html", () => {
+    return gulp
+        .src("src/example/*.html")
+        .pipe(newer("build"))
+        .pipe(gulp.dest("build"))
+        .pipe(browserSync.stream());
+});
+gulp.task("html-min", () => {
+    return gulp
+        .src("src/example/*.html")
+        .pipe(htmlmin({ collapseWhitespace: true, removeComments: true }))
+        .pipe(gulp.dest("deploy"))
+        .pipe(browserSync.stream())
 });
 
 /* ----------------- * /
@@ -161,32 +141,82 @@ gulp.task('html', ['cssmin'], () => {
 /* ----------------- */
 gulp.task("fonts", () => {
     gulp
-        .src(["src/example/css/fonts", "src/example/css/images"])
+        .src(["src/example/css/fonts/**/*"])
         .pipe(
             plumber({
                 errorHandler: onError
             })
         )
-        .pipe(gulp.dest("build/css"));
+        .pipe(changed("build/css/fonts"))
+        .pipe(gulp.dest("build/css/fonts"));
 });
 gulp.task("fonts-min", () => {
     gulp
-        .src(["src/example/css/fonts", "src/example/css/images"])
+        .src(["src/example/css/fonts/**/*"])
         .pipe(
             plumber({
                 errorHandler: onError
             })
         )
-        .pipe(gulp.dest("deploy/css"));
+        .pipe(changed("deploy/css/fonts"))
+        .pipe(gulp.dest("deploy/css/fonts"));
+});
+
+/* ----------------- * /
+/* CSS images
+/* ----------------- */
+gulp.task("css-images", () => {
+    gulp
+        .src(["src/example/css/images/**/*"])
+        .pipe(
+            plumber({
+                errorHandler: onError
+            })
+        )
+        .pipe(gulp.dest("build/css/images"));
+});
+gulp.task("css-images-min", () => {
+    gulp
+        .src(["src/example/css/images/**/*"])
+        .pipe(
+            plumber({
+                errorHandler: onError
+            })
+        )
+        .pipe(gulp.dest("deploy/css/images"));
+});
+
+
+/* ----------------- * /
+/* Other root folders
+/* ----------------- */
+gulp.task("folders", () => {
+    gulp
+        .src(["src/example/libs/**/*"])
+        .pipe(
+            plumber({
+                errorHandler: onError
+            })
+        )
+        .pipe(gulp.dest("build/libs"));
+});
+gulp.task("folders-min", () => {
+    gulp
+        .src(["src/example/libs/**/*"])
+        .pipe(
+            plumber({
+                errorHandler: onError
+            })
+        )
+        .pipe(gulp.dest("deploy/libs"));
 });
 
 /* ----------------- */
-/* Images
+/* IQuiz assets
 /* ----------------- */
-gulp.task("images", () => {
-    var imgSrc = "./src/example/images/**/*",
-        imgDst = "./build/images/";
-
+gulp.task("iquiz", () => {
+    var imgSrc = ["./src/example/iquiz_assets/**/*"],
+        imgDst = "./build/iquiz_assets";
     return (
         gulp
             .src(imgSrc)
@@ -196,13 +226,13 @@ gulp.task("images", () => {
                     errorHandler: onError
                 })
             )
+            .pipe(changed(imgDst))
             .pipe(gulp.dest(imgDst))
     );
 });
-gulp.task("images-min", () => {
-    var imgSrc = "./src/example/images/**/*",
-        imgDst = "./deploy/images/";
-
+gulp.task("iquiz-min", () => {
+    var imgSrc = ["./src/example/iquiz_assets/**/*"],
+        imgDst = "./deploy/iquiz_assets";
     return (
         gulp
             .src(imgSrc)
@@ -212,44 +242,106 @@ gulp.task("images-min", () => {
                     errorHandler: onError
                 })
             )
-            //.pipe(changed(imgDst))
-            //.pipe(imagemin())
+            .pipe(changed(imgDst))
+            .pipe(imagemin())
             .pipe(gulp.dest(imgDst))
         //.pipe(notify({ message: "Images task complete" }))
     );
 });
 
+
 /* ----------------- */
-/* Cssmin
+/* Images
 /* ----------------- */
-gulp.task('default', function () {
-    gulp.src('src/**/*.css')
-        .pipe(cssmin())
-        .pipe(rename({ suffix: '.min' }))
-        .pipe(gulp.dest('dist'));
+gulp.task("images", () => {
+    var imgSrc = ["./src/example/images/**/*"],
+        imgDst = "./build/images";
+    return (
+        gulp
+            .src(imgSrc)
+            .pipe(newer(imgDst))
+            .pipe(
+                plumber({
+                    errorHandler: onError
+                })
+            )
+            .pipe(changed(imgDst))
+            .pipe(gulp.dest(imgDst))
+    );
 });
-
-
-gulp.task('cssmin', () => {
-    return gulp.src('./client/scss/**/*.scss')
-        .pipe(plugins().sass({
-            'outputStyle': 'compressed'
-        }).on('error', plugins().sass.logError))
-        .pipe(gulp.dest('./build/css/'));
+gulp.task("images-min", () => {
+    var imgSrc = ["./src/example/images/**/*"],
+        imgDst = "./deploy/images";
+    return (
+        gulp
+            .src(imgSrc)
+            .pipe(newer(imgDst))
+            .pipe(
+                plumber({
+                    errorHandler: onError
+                })
+            )
+            .pipe(changed(imgDst))
+            .pipe(imagemin())
+            .pipe(gulp.dest(imgDst))
+        //.pipe(notify({ message: "Images task complete" }))
+    );
 });
-
-
-
-
-/* ----------------- */
-/* Taks
-/* ----------------- */
-
 
 
 /* ----------------- */
 /* Build Pack
 /* ----------------- */
+gulp.task("copy-pack", () => {
+
+    if (!fs.existsSync('pack'))
+        fs.mkdirSync('pack'), console.log("📁  folder created:", 'pack');
+
+    let jsBundleStreams = [];
+    // Create pack folder and Contents folder
+    if (!fs.existsSync(packDir))
+        fs.mkdirSync(packDir), console.log("📁  folder created:", packDir);
+
+    // Add content pack files
+    let packSrc = `src/imsmanifest/**`,
+        packDest = `${packDir}`;
+    jsBundleStreams.push(
+        gulp
+            .src(packSrc)
+            .pipe(newer(packDest))
+            .pipe(plumber({ errorHandler: onError }))
+            .pipe(gulp.dest(packDest))
+    );
+    return merge(jsBundleStreams);
+})
+gulp.task("copy-content", () => {
+    let jsBundleStreams = [];
+    let contentDir = packDir + "/contents";
+    if (!fs.existsSync(contentDir))
+        fs.mkdirSync(contentDir), console.log("📁  folder created:", contentDir);
+
+    // Add dest files to Content folder
+    let filesSrc = `deploy/**`,
+        filesDist = `${contentDir}`;
+    jsBundleStreams.push(
+        gulp
+            .src(filesSrc)
+            //.pipe(newer(filesDist))
+            .pipe(plumber({ errorHandler: onError }))
+            .pipe(gulp.dest(filesDist))
+    );
+    return merge(jsBundleStreams);
+})
+gulp.task("create-zip", () => {
+    let jsBundleStreams = [];
+    jsBundleStreams.push(
+        gulp.src(`${packDir}/**`)
+            .pipe(zip(`${packDir}.zip`))
+            .pipe(gulp.dest('./'))
+    )
+    return merge(jsBundleStreams);
+})
+
 gulp.task("build-pack", () => {
 
     if (!fs.existsSync("pack"))
@@ -306,6 +398,8 @@ gulp.task("build-pack", () => {
                 .pipe(plumber({ errorHandler: onError }))
                 .pipe(gulp.dest(filesDist))
         );
+
+
         jsBundleStreams.push(
             gulp.src(`${dir}/*`)
                 .pipe(zip(`${folder}.zip`))
@@ -330,10 +424,10 @@ gulp.task('clean', function () {
 /* ----------------- */
 /* Taks
 /* ----------------- */
-gulp.task('development', ['scripts', 'sass', 'fonts', 'images'], () => {
+gulp.task('development', ['scripts', 'sass', 'fonts', 'css-images', 'images', 'html', 'folders', 'iquiz'], () => {
     browserSync({
         'server': './',
-        startPath: "build/index.html#/task1/slides",
+        startPath: "build/index.html#/task1/slides/0",
         'snippetOptions': {
             'rule': {
                 'match': /<\/body>/i,
@@ -341,13 +435,19 @@ gulp.task('development', ['scripts', 'sass', 'fonts', 'images'], () => {
             }
         }
     });
-    gulp.watch('./src/ebook/scss/**/*.scss', ['sass', browserSync.reload]);
+    gulp.watch('./src/ebook/scss/**/*.scss', ['styles', browserSync.reload]);
+    gulp.watch('./src/iquiz/scss/**/*.scss', ['styles', browserSync.reload]);
     gulp.watch('./src/ebook/js/**/*.js', ['scripts', browserSync.reload]);
     gulp.watch('./src/iquiz/js/**/*.js', ['scripts', browserSync.reload]);
     gulp.watch('./src/example/js/**/*.js', ['scripts', browserSync.reload]);
-    gulp.watch('./build/*.html', browserSync.reload);
+    gulp.watch('./src/example/css/**/*.scss', ['styles', browserSync.reload]);
+    gulp.watch('./src/example/*.html', ['html', browserSync.reload]);
 });
 
 gulp.task('default', ['development']);
-gulp.task('deploy', ['scripts-min', 'sass-min', 'fonts-min', 'images-min']);
-gulp.task("pack", ["clean", "build-pack"]);
+gulp.task('deploy', ['scripts-min', 'sass-min', 'fonts-min', 'css-images-min', 'images-min', 'html-min', 'folders-min', 'iquiz-min']);
+//gulp.task("pack", ["clean", "build-pack"]);
+
+gulp.task('pack', function (callback) {
+    runSequence('clean', 'copy-pack', 'copy-content', 'create-zip', callback);
+});
